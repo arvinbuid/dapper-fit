636bcd91e465983e6d0307d9cb6a05d43cfe644b
Arvin Buid <arvinbuid01@gmail.com>
2024-12-31 11:11:35 +0800

    Created Product Card Component to render the products in the homepage.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import ProductList from "@/components/shared/products/product-list";
+import ProductList from "@/components/shared/product/product-list";
 import sampleData from "@/db/sample-data";
 
 const Homepage = async () => {
