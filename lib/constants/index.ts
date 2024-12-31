7c7eec60c4d6f28cc7a9aea7a1b47cef1f77ab37
Arvin Buid <arvinbuid01@gmail.com>
2024-12-31 21:33:40 +0800

    Implemented loading the products coming from the database, created helper function convertToPlainObject and make the product fetching limit constant.

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,3 +4,4 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "A modern ecommerce project application built using Next.js.";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
