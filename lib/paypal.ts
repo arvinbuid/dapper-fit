5d575f8360ed56d805e8921570502956a0851dd5
Arvin Buid <arvinbuid01@gmail.com>
2025-01-07 23:11:12 +0800

    Fixed typo.

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -30,7 +30,7 @@ export const paypal = {
   },
   capturePayment: async function capturePayment(orderId: string) {
     const accessToken = await generateAcessToken();
-    const url = `/v2/checkout/orders/${orderId}/capture`;
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
 
     const response = await fetch(url, {
       method: "POST",
