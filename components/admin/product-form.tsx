5a2cb430637216adbda9ab173625c24b42c7091e
Arvin Buid <arvinbuid01@gmail.com>
2025-01-12 22:05:22 +0800

    Implemented making the product to be created is featured and become a banner image.

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -17,6 +17,7 @@ import {createProduct, updateProduct} from "@/lib/actions/product.actions";
 import {UploadButton} from "@/lib/uploadthing";
 import {Card, CardContent} from "../ui/card";
 import Image from "next/image";
+import {Checkbox} from "../ui/checkbox";
 
 const AdminProductForm = ({
   type,
@@ -82,6 +83,8 @@ const AdminProductForm = ({
   };
 
   const images = form.watch("images");
+  const isFeatured = form.watch("isFeatured");
+  const banner = form.watch("banner");
 
   return (
     <>
@@ -259,7 +262,50 @@ const AdminProductForm = ({
               )}
             />
           </div>
-          <div className='upload-field'>{/* isFeatured */}</div>
+          <div className='upload-field'>
+            {/* isFeatured & banner */}
+            Featured Product
+            <Card className='mt-2'>
+              <CardContent className='space-y-2 mt-2'>
+                <FormField
+                  control={form.control}
+                  name='isFeatured'
+                  render={({field}) => (
+                    <FormItem className='space-x-2 items-center'>
+                      <FormControl>
+                        <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                      </FormControl>
+                      <FormLabel>Is Featured?</FormLabel>
+                    </FormItem>
+                  )}
+                />
+                {isFeatured && banner && (
+                  <Image
+                    src={banner}
+                    alt='product banner'
+                    className='w-full object-cover object-center rounded-sm'
+                    width={1920}
+                    height={680}
+                    unoptimized
+                  />
+                )}
+                {isFeatured && !banner && (
+                  <UploadButton
+                    endpoint='imageUploader'
+                    onClientUploadComplete={(res: {url: string}[]) => {
+                      form.setValue("banner", res[0].url);
+                    }}
+                    onUploadError={(error: Error) => {
+                      toast({
+                        variant: "destructive",
+                        description: `ERROR! ${error.message}`,
+                      });
+                    }}
+                  />
+                )}
+              </CardContent>
+            </Card>
+          </div>
           <div>
             {/* Description */}
             <FormField
