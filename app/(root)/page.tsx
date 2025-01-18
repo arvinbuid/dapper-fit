4623b55f87f7f35613482560f5cbe1a552530e25
Arvin Buid <arvinbuid01@gmail.com>
2025-01-18 14:26:38 +0800

    Created deal countdown component.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import DealCountdown from "@/components/deal-countdown";
+import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/shared/view-all-products-button";
@@ -11,7 +13,8 @@ const Homepage = async () => {
       {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title='Newest Arrivals' />
       <ViewAllProductsButton />
-      Iconrpx
+      <IconBoxes />
+      <DealCountdown />
     </>
   );
 };
