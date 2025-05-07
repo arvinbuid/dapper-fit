6cb8aa6b2caaea0392d4cb24868abd9325abe67f
Arvin Buid <arvinbuid01@gmail.com>
2025-05-08 00:37:23 +0800

    Fixed bug where any users can see another users order.

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,9 +1,9 @@
-import {Metadata} from "next";
-import {getOrderById} from "@/lib/actions/order.actions";
-import {notFound} from "next/navigation";
+import { Metadata } from "next";
+import { getOrderById } from "@/lib/actions/order.actions";
+import { notFound, redirect } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
-import {ShippingAddress} from "@/types";
-import {auth} from "@/auth";
+import { ShippingAddress } from "@/types";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -13,12 +13,17 @@ const OrderDetailsPage = async (props: {
     id: string;
   }>;
 }) => {
-  const {id} = await props.params;
-
-  const session = await auth();
+  const { id } = await props.params;
 
   const order = await getOrderById(id);
   if (!order) notFound();
+
+  const session = await auth();
+
+  // Redirect the user if they don't own the order
+  if (order.userId !== session?.user.id && session?.user.role !== 'admin') {
+    return redirect('/unauthorized');
+  }
 
   return (
     <>
